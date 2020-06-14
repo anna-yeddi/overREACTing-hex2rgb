fb45a849f3f331de0f18d33ed4bcf485b98aa35e
Anna Kova <m3ta007@gmail.com>
2020-06-14 01:04:45 -0400

    Dynamics is added

--- src/components/Hex2rgb.jsx
+++ src/components/Hex2rgb.jsx
@@ -1,28 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 function Hex2rgb(props) {
-  const hexDefault = '#34495e'
+  const hexRegex = new RegExp('^#[0-9a-f]{6}', 'i')
+  const [hex, setHex] = useState('')
+
+  const handleChange = ({ target }) => {
+    setHex(target.value)
+    console.log(hex)
+  }
+
+  // Validate input
+  // and run converter only when its length is 7
+  const hexUpdated = (hex) => {
+    if (!hexRegex.test(hex)) {
+      return 'error'
+    }
+    return hex.length === 7 && hex
+  }
+
+  // Convert HEX to RGB values:
+  const rgbFrom = (hex) => {
+    if (hex === 'error') {
+      return (
+        <span className="error" id="hex-error">
+          Error: invalid HEX
+        </span>
+      )
+    } else {
+      let r = 0,
+        g = 0,
+        b = 0
+
+      if (hex.length === 7) {
+        r = '0x' + hex[1] + hex[2]
+        g = '0x' + hex[3] + hex[4]
+        b = '0x' + hex[5] + hex[6]
+      }
+
+      return 'rgb(' + +r + ',' + +g + ',' + +b + ')'
+    }
+  }
 
   return (
-    <div className="container" style={{ backgroundColor: { hexDefault } }}>
+    <div className="container" style={{ backgroundColor: hexUpdated(hex) }}>
       <label htmlFor="hex-input" className="hex-input-label">
         HEX <span className="hex-input-inner">Code</span>
       </label>
       <input
         name="hex"
         id="hex-input"
-        value="#34495e"
+        value={hex.value}
         className="input-text"
         aria-describedby="hex-error"
         placeholder="#xxxxxx"
-        onChange={console.log('changed!')}
+        onChange={handleChange}
+        maxLength="7"
         required
       />
-      <div className="result">rgb(52, 73, 94)</div>
-      <div className="error" id="hex-error">
-        Error!
-      </div>
+      <div className="result">{rgbFrom(hexUpdated(hex))}</div>
     </div>
   )
 }
