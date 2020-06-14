3b169d4e4ba36357045539e063f30888ded3d44c
Anna Kova <m3ta007@gmail.com>
2020-06-14 14:07:08 -0400

    OnChange logic is added

--- src/components/Hex2rgb.jsx
+++ src/components/Hex2rgb.jsx
@@ -2,48 +2,41 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 function Hex2rgb(props) {
-  const hexRegex = new RegExp('^#[0-9a-f]{6}', 'i')
-  const [hex, setHex] = useState('')
+  const [hex, setHex] = useState({
+    value: '',
+    isValid: true,
+  })
 
+  // Update state and validate input
   const handleChange = ({ target }) => {
-    setHex(target.value)
-    console.log(hex)
-  }
+    const hexRegex = new RegExp('^#[0-9a-f]{6}', 'i')
+    const isValidValue =
+      hexRegex.test(target.value) && target.value.length === 7
 
-  // Validate input
-  // and run converter only when its length is 7
-  const hexUpdated = (hex) => {
-    if (!hexRegex.test(hex)) {
-      return 'error'
-    }
-    return hex.length === 7 && hex
+    setHex(() => ({
+      value: target.value,
+      isValid: isValidValue,
+    }))
+    console.log(hex)
   }
 
   // Convert HEX to RGB values:
   const rgbFrom = (hex) => {
-    if (hex === 'error') {
-      return (
-        <span className="error" id="hex-error">
-          Error: invalid HEX
-        </span>
-      )
-    } else {
-      let r = 0,
-        g = 0,
-        b = 0
+    let r = 0,
+      g = 0,
+      b = 0
 
-      if (hex.length === 7) {
-        r = '0x' + hex[1] + hex[2]
-        g = '0x' + hex[3] + hex[4]
-        b = '0x' + hex[5] + hex[6]
-      }
-
-      return 'rgb(' + +r + ',' + +g + ',' + +b + ')'
+    if (hex.length === 7) {
+      r = '0x' + hex[1] + hex[2]
+      g = '0x' + hex[3] + hex[4]
+      b = '0x' + hex[5] + hex[6]
     }
+
+    return 'rgb(' + +r + ',' + +g + ',' + +b + ')'
   }
 
   return (
-    <div className="container" style={{ backgroundColor: hexUpdated(hex) }}>
+    <div className="container" style={{ backgroundColor: rgbFrom(hex.value) }}>
       <label htmlFor="hex-input" className="hex-input-label">
         HEX <span className="hex-input-inner">Code</span>
       </label>
@@ -52,13 +45,18 @@ function Hex2rgb(props) {
         id="hex-input"
         value={hex.value}
         className="input-text"
-        aria-describedby="hex-error"
+        aria-describedby="hex-result"
+        aria-valid={hex.isValid}
         placeholder="#xxxxxx"
-        onChange={handleChange}
         maxLength="7"
+        onChange={handleChange}
         required
       />
-      <div className="result">{rgbFrom(hexUpdated(hex))}</div>
+      <div
+        className={`result result-${hex.isValid ? 'rgb' : 'error'}`}
+        id="hex-result">
+        {hex.isValid ? rgbFrom(hex.value) : 'Error: invalid HEX code'}
+      </div>
     </div>
   )
 }
